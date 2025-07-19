43f6d364eabc571f6f7476ee11006cf24de9808f
himalayashinde@gmail.com <himalayashinde@gmail.com>
2025-07-19 18:41:53 +0530

    Grocery Store Purchase

---
+++ PythonBasics/grocery_store_purchase.py
@@ -0,0 +1,15 @@
+# Grocery Store Purchase - Subtotal Calculator
+
+# Item prices
+penne_price = 16.68  # Penne 16 oz Pack of 12
+sauce_price = 6.98  # Arrabiata Pasta Sauce 24 oz
+garlic_price = 16.78  # Bag of 20 Organic Garlic Cloves
+seasoning_price = 15.26  # Italian Seasoning 1.5 oz Bottle
+baguettes_price = 3.00  # Artisan Baguettes Twin Pack
+meatballs_price = 4.39  # 12 oz Bag of Meatballs
+
+# Calculating subtotal using an expression
+subtotal = penne_price + sauce_price + garlic_price + seasoning_price + baguettes_price + meatballs_price
+
+# Printing the subtotal
+print("Subtotal of all 6 items: $", subtotal)
---
+++ PythonBasics/print_excercise.py
@@ -0,0 +1,14 @@
+# print() Exercises
+
+# 1. Create a variable, assign it a value of any data type, then print it
+name = "Himalaya"  # Assigning a string value to the variable 'name'
+print(name)  # Printing the value of 'name'
+
+# 2. Use print() to print a value of any data type directly
+print(42)  # Printing an integer directly
+print(3.14)  # Printing a float directly
+print(True)  # Printing a boolean value directly
+
+# 3. Use print() to display the result of an expression that uses at least 2 math operators
+print(10 + 5 * 2)  # Multiplication has higher precedence; result is 10 + (5*2) = 20
+    
