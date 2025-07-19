6ccb296b54bf9b2c9f0bf11633b80834bfd6b8b8
himalayashinde@gmail.com <himalayashinde@gmail.com>
2025-07-19 18:20:16 +0530

    Python Basics

---
+++ PythonBasics/Variable.py
@@ -0,0 +1,32 @@
+# Sum of two numbers
+
+a= 5
+b= 6
+c= a+b
+print(f"a={a},b={b}, c= a+b i.e {c}")
+
+a= 10
+
+print(f"a={a}")
+
+
+# Variables and Assignment Exercises
+# Do all of this in a .py file in Pycharm
+# create a variable and assign it an integer
+
+var_int= 20
+print(type(var_int))
+print(var_int)
+
+# create a second variable and assign it a float
+var_float= 3.14
+print(type(var_float))
+
+# create a third variable and assign it a Boolean value
+var_bool = True
+print(type(var_bool))
+
+# re-assign a different integer to the first variable you created
+var_int= 30
+print(type(var_int))
+print(var_int)
